OpenPLi/openpli-oe-core

Thud: Fix f4mdump compile error

meta-openpli/recipes-multimedia/f4mdump/f4mdump_git.bb
@@ -1,6 +1,8 @@
 DESCRIPTION = "Simple F4M/HDS (Adobe HTTP Dynamic Streaming) dumper. Intended to be used by IPTVPlayer plugin: https://gitorious.org/iptv-pl-dla-openpli, http://iptvplayer.vline.pl."
 MAINTAINER = "samsamsam"
 
+FILESEXTRAPATHS_prepend := "${THISDIR}/$PN}:"
+
 DEPENDS = "openssl zlib"
 RDEPENDS_${PN} += "wget"
 
@@ -11,7 +13,8 @@ PV = "1.0+git${SRCPV}"
 PKGV = "1.0+git${GITPKGV}"
 PR = "r0"
 
-SRC_URI = "git://gitlab.com/f4mdump/f4mdump.git;protocol=http"
+SRC_URI = "git://gitlab.com/f4mdump/f4mdump.git;protocol=http \
+	file://fix-build-openssl102q.patch"
 
 S = "${WORKDIR}/git/"
 

Toysoft/openpli-oe-core_20200518

meta-openpli/recipes-multimedia/f4mdump/f4mdump_git.bb
@@ -1,6 +1,8 @@
 DESCRIPTION = "Simple F4M/HDS (Adobe HTTP Dynamic Streaming) dumper. Intended to be used by IPTVPlayer plugin: https://gitorious.org/iptv-pl-dla-openpli, http://iptvplayer.vline.pl."
 MAINTAINER = "samsamsam"
 
+FILESEXTRAPATHS_prepend := "${THISDIR}/$PN}:"
+
 DEPENDS = "openssl zlib"
 RDEPENDS_${PN} += "wget"
 
@@ -11,7 +13,8 @@ PV = "1.0+git${SRCPV}"
 PKGV = "1.0+git${GITPKGV}"
 PR = "r0"
 
-SRC_URI = "git://gitlab.com/e2i/f4mdump.git;protocol=http"
+SRC_URI = "git://gitlab.com/e2i/f4mdump.git;protocol=http \
+	file://fix-build-openssl102q.patch"
 
 S = "${WORKDIR}/git/"
 